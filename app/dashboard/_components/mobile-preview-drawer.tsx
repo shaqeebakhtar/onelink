be01ae2b622251016ada3a066bbe7a82bb1b7e3e
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2024-02-07 15:18:23 +0530

    preview for mobile

---
+++ app/dashboard/_components/mobile-preview-drawer.tsx
@@ -0,0 +1,29 @@
+import { Button } from '@/components/ui/button';
+import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer';
+import { Eye } from 'lucide-react';
+
+const MobilePreviewDrawer = () => {
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <div className="fixed left-1/2 -translate-x-1/2 bottom-8">
+          <Button className="shadow rounded-full px-5 h-11 bg-gray-200 text-foreground hover:bg-primary hover:text-primary-foreground">
+            <Eye className="w-5 h-5 mr-2" strokeWidth={1.5} />
+            <span className="text-base font-medium">Preview</span>
+          </Button>
+        </div>
+      </DrawerTrigger>
+      <DrawerContent className="h-[calc(100vh-48px)]">
+        <div className="grid place-items-center px-2 py-3 w-full h-full overflow-hidden">
+          <iframe
+            className="w-full h-full border-none rounded-2xl"
+            src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"
+            title="Preview"
+          ></iframe>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default MobilePreviewDrawer;
--- app/dashboard/_components/preview-container.tsx
+++ app/dashboard/_components/preview-container.tsx
@@ -1,15 +1,25 @@
+'use client';
+import { useMediaQuery } from 'usehooks-ts';
+import MobilePreviewDrawer from './mobile-preview-drawer';
+
 const PreviewContainer = () => {
-  return (
-    <div className="fixed md:mt-12 lg:mt-8 max-w-80 lg:max-w-[540px] md:max-w-72 w-full right-0 top-0 bottom-0 z-0 h-full p-8 hidden md:grid place-items-center border-l border-gray-200">
-      <div className="max-w-[275px] h-full md:max-h-[450px] lg:max-h-[540px] border-[12px] border-gray-400 rounded-[40px] overflow-hidden select-none shadow-md">
-        <iframe
-          className="w-full h-full border-none"
-          src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"
-          title="Preview"
-        ></iframe>
+  const destopOrTab = useMediaQuery('(min-width: 768px)');
+
+  if (destopOrTab) {
+    return (
+      <div className="fixed md:mt-12 lg:mt-8 max-w-80 lg:max-w-[540px] md:max-w-72 w-full right-0 top-0 bottom-0 z-0 h-full p-8 hidden md:grid place-items-center border-l border-gray-200">
+        <div className="max-w-[275px] h-full md:max-h-[450px] lg:max-h-[540px] border-[12px] border-gray-400 rounded-[40px] overflow-hidden select-none shadow-md">
+          <iframe
+            className="w-full h-full border-none"
+            src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"
+            title="Preview"
+          ></iframe>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <MobilePreviewDrawer />;
 };
 
 export default PreviewContainer;
