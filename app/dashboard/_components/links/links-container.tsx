225e71c4515a94e1ca83ca2671180182df88d443
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2024-02-15 01:21:09 +0530

    tanstack query setup

--- app/dashboard/_components/links/links-container.tsx
+++ app/dashboard/_components/links/links-container.tsx
@@ -3,9 +3,21 @@ import { DndContext, DragEndEvent } from '@dnd-kit/core';
 import { SortableContext, arrayMove } from '@dnd-kit/sortable';
 import React, { useId, useState } from 'react';
 import LinkCard from './link-card';
+import { useQuery } from '@tanstack/react-query';
 
 const LinksContainer = () => {
   const [items, setItems] = useState(['1', '2', '3', '4']);
+
+  // const query = useQuery({
+  //   queryKey: ['todos'],
+  //   queryFn: async () => {
+  //     const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  //     const data = res.json();
+  //     return data;
+  //   },
+  // });
+
+  // console.log(query.data);
 
   const handleDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 };
 
 export default Providers;
